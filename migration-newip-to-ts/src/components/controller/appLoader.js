44fa9cc045f0523da14cb92a6baa00dc2e35255b
gavrilenkoartem <gavrilenkoartem038@gmail.com>
2022-07-01 15:33:15 +0400

    feat: add api key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'd0e53456cdff4a80807662492474aeb1',
         });
     }
 }
