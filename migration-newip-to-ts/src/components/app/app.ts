178140a0a2044988e57b03a939e2c08b53a34213
gavrilenkoartem <gavrilenkoartem038@gmail.com>
2022-07-04 02:37:45 +0400

    feat: add alphabet sort of sources

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -2,19 +2,23 @@ import { INews } from '../../types/interface';
 import { ISources } from '../../types/interface';
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import Sources from '../view/sources/sources';
 
 class App {
     controller: AppController;
     view: AppView;
+    sources: Sources;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
+        this.sources = new Sources();
     }
 
     start() {
         (document.querySelector('.sources') as HTMLElement).addEventListener('click', (e) =>
             this.controller.getNews(e, (data: INews) => this.view.drawNews(data))
         );
+        (document.querySelector('main') as HTMLElement).addEventListener('click', (e) => this.sources.show(e));
         this.controller.getSources((data: ISources) => this.view.drawSources(data));
     }
 }
--- migration-newip-to-ts/src/components/view/sources/sources.ts
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -4,7 +4,9 @@ import { ISource } from '../../../types/interface';
 class Sources {
     draw(data: ISource[]) {
         const fragment = document.createDocumentFragment();
+        const symbolsContainer = document.querySelector('.symbols-container') as HTMLElement;
         const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+        const firstCharArr: string[] = [];
 
         data.forEach((item: ISource) => {
             const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
@@ -13,9 +15,33 @@ class Sources {
             (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
-        });
 
-        (document.querySelector('.sources') as HTMLElement).append(fragment);
+            if (!firstCharArr.includes(item.name[0])) {
+                firstCharArr.push(item.name[0]);
+            }
+            (document.querySelector('.sources') as HTMLElement).append(fragment);
+        });
+        firstCharArr.forEach((elem) => {
+            const symbolDiv = document.createElement('div');
+            symbolsContainer.append(symbolDiv);
+            symbolDiv.textContent = elem;
+            symbolDiv.id = elem;
+            symbolDiv.classList.add('symbol-category');
+        });
+    }
+
+    show(e: Event) {
+        const elem = e.target as HTMLElement;
+        console.log(elem);
+        if (elem.classList.contains('symbol-category')) {
+            const elements = document.querySelectorAll('.source__item');
+            elements.forEach((el) => {
+                el.classList.add('hidden');
+                if ((el.getAttribute('data-source-id') as string)[0] == elem.id.toLowerCase()) {
+                    el.classList.remove('hidden');
+                }
+            });
+        }
     }
 }
 
