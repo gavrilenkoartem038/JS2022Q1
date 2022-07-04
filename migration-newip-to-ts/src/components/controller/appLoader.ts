afd9f04aa6766eda8db8c63f8bc49772aec4ea57
gavrilenkoartem <gavrilenkoartem038@gmail.com>
2022-07-04 14:14:32 +0400

    fix: change link for deploy

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'd0e53456cdff4a80807662492474aeb1',
         });
     }
